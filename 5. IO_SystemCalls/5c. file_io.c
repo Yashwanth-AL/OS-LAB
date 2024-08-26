af0b4fc85d3e9bf62bd0fe4f4b7e935c0fe49c91
Yashwanth-AL <yashwanthal2004@gmail.com>
2024-08-27 00:03:26 +0530

    updated codes

--- 5. IO_SystemCalls/5c. file_io.c
+++ 5. IO_SystemCalls/5c. file_io.c
@@ -1,15 +1,22 @@
-//-------------use of I/O system calls for File I/O//
+/*Write a program to perform the following task using I/O system calls 
+for file I/O 
+i.  Reading first 10 characters from file 
+ii.  Skipping 5 characters from current position in the file 
+iii.  Going to 5th  last character in the file 
+iv.  Going to the 3rd character in the file*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define BUFFER_SIZE 30
 
 int main() {
-    int fd = open("./file.txt", O_RDWR), n;
-    char buffer[BUFFER_SIZE];
+    int fd = open("./file.txt", O_RDWR);
+    int n;
+    char buffer[50];
     printf("File descriptor is %d\n", fd);
     if (fd == -1) {
         printf("Failed to open file.\n");
@@ -22,10 +29,10 @@ int main() {
     lseek(fd, 5, SEEK_CUR);
     n = read(fd, buffer, 10);
     write(1, buffer, n);
-    printf("\nGoing 10 characters before the current position in the file:\n");
-    lseek(fd, -10, SEEK_CUR);
-    n = read(fd, buffer, 10);
-    write(1, buffer, n);
+    // printf("\nGoing 10 characters before the current position in the file:\n");
+    // lseek(fd, -10, SEEK_CUR);
+    // n = read(fd, buffer, 10);
+    // write(1, buffer, n);
     printf("\nGoing to 5th last character in the file:\n");
     lseek(fd, -5, SEEK_END);
     n = read(fd, buffer, 5);
